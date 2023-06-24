58278ebcaa11d448cb6b0c47be62117183d294bb
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-06-23 22:24:49 -0400

    starting infinite scroll

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import Pokemon from "./Pokemon";
 import "./Pokemons.css";
+const LIMIT = 20;
+
 const Pokemons = () => {
   const [pokemons, setPokemons] = useState([]);
 
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const getPokemons = () => {
-      fetch(" https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+    const getPokemons = async () => {
+      fetch(
+        ` https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page * LIMIT}`
+      )
         .then((res) => res.json())
         .then((data) => {
           console.log(data.results);
@@ -14,7 +21,27 @@ const Pokemons = () => {
         });
     };
     getPokemons();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => window.removeEventListener("scroll", handleInfiniteScroll);
   }, []);
+
+  const handleInfiniteScroll = async () => {
+    try {
+      if (
+        window.innerHeight + window.document.documentElement.scrollTop + 1 >=
+        window.document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main>
       <h1>Pokemons</h1>
